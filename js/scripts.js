3a60a5399f5eb3ab8fd4754dc855815196612571
Matt Eilar <meilar@gmail.com>
2022-01-14 13:29:19 -0800

    add waiting screen, timed transitions, and branching logic for results

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  let userScore = 0;
+
   $("button#btn-go").click(function() {
     console.log("load and button is working");
     $("div.intro").hide();
@@ -6,7 +8,26 @@ $(document).ready(function() {
     $("button#btn-submit").show();
   });
 
+  $("button#btn-submit").click(function() {
+    $("div.questions").toggle();
+    $("div.waiting").show();
+    setTimeout(function() {
+      $("div.waiting").toggle();
+      
+      if (userScore >= 5) {
+        $("div#ans-3").show();
+      } else if (userScore === 4 || userScore === 5) {
+        $("div#ans-2").show();  
+      } else {
+        $("div#ans-1").show();
+      }
+
+    }, 3000);
+
+  });
+
   $("button#btn-reset").click(function() {
     location.reload();
   });
+
 });
