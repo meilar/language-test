73421b7a26627b6b8f82f9048c93be4257e54ac4
Matt Eilar <meilar@gmail.com>
2022-01-14 12:05:21 -0800

    refactor css and add transition buttons

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
     console.log("load and button is working");
     $("div.intro").hide();
     $("div.questions").show();
+    $("button#btn-submit").show();
   });
+
   $("button#btn-reset").click(function() {
     location.reload();
   });
