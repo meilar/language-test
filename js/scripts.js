05405ad5cd34821c5c2bf861bffca1ef916b6822
Matt Eilar <meilar@gmail.com>
2022-01-14 15:18:46 -0800

    add goodbye message based on user input from question 6

--- js/scripts.js
+++ js/scripts.js
@@ -13,19 +13,37 @@ $(document).ready(function() {
     setTimeout(function() {
       $("div.waiting").toggle();
 
+      const userEnthusiam = parseInt($("#q6-range").val());
+
+      if (userEnthusiam === 1) {
+        $(".ans-final-1").text("I'm sorry to hear that you are not excited about your career transition. If I was staring down the barrel of an entire career spent using ");
+        $(".ans-final-2").text(", I wouldn't be excited either. Good luck!");
+      } else if (userEnthusiam === 2) {
+        $(".ans-final-1").text("I'm glad to hear that you are somewhat excited. Did you see that your best match was ");
+        $(".ans-final-2").text("? It might be time to lower those expectations. Good luck!");
+      } else {
+        $(".ans-final-1").text("It's fantastic that you are so excited about your career transition. I expect that after working with ");
+        $(".ans-final-2").text(" that will go away pretty quickly. Good luck!");
+      }
+
     const lunch = parseInt($("#select-lunch").val());  
 
     if (lunch === 3) {
+      $("span.lang-ins").text("Deadfish");
       $("div#ans-3").show();
     } else if (lunch === 2) {
-      $("div#ans-2").show();  
+      $("span.lang-ins").text("LOLCODE");  
+      $("div#ans-2").show();
     } else {
+      $("span.lang-ins").text("Airline Food");
       $("div#ans-1").show();
     }
   
+
+
     const userColor = $("#q4-color").val();
 
-    $(".lang-name").css("color", userColor);
+    $("span.lang-name").css("color", userColor);
       
 
 
